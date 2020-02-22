0d68f50bd4290ebce6e05610d4e7166763d7d82b
Jens Klingenberg <mail@jensklingenberg.de>
2020-02-22 23:39:14 +0100

    init commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/jens/Code/IDE/FlutterSDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/jens/Code/2020/Trash/flut/examples/flutter_view"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/jens/Code/IDE/FlutterSDK/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
